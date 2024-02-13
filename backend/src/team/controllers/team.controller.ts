8d860f62b5e5f03ff9ed17df9f73cc2e2edd01b5
guusfelix2015 <gusfelix2015@gmail.com>
2024-02-13 13:07:18 -0300

    feat(api): getOne team

--- backend/src/team/controllers/team.controller.ts
+++ backend/src/team/controllers/team.controller.ts
@@ -21,6 +21,11 @@ export class TeamController {
     return this.teamService.getAll();
   }
 
+  @Get(':id')
+  async getOne(@Param('id') id: string) {
+    return this.teamService.getOne(Number(id));
+  }
+
   @Post()
   async create(@Body() body: CreateTeamDto) {
     return this.teamService.create(body);
